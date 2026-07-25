51a35e2651e73b30dda5354f1c935c40b300dca2
moo-farah <rizmik.farah@gmail.com>
2026-07-25 23:05:41 +0300

    updated the photos and deleted one photo

--- src/data/photos.ts
+++ src/data/photos.ts
@@ -8,23 +8,23 @@ export const photos = [
   },
   {
     image: "https://raw.githubusercontent.com/moo-farah/personal-site/main/src/assets/images/2.jpeg",
-    date: "December 2024",
+    date: "May 2024",
     location: "Future Museum",
     caption: "dubai future museum was future",
     style: "sm:top-12 sm:left-[15%] top-8 right-2 rotate-[4deg] z-20"
   },
   {
-    image: "https://raw.githubusercontent.com/moo-farah/personal-site//main/src/assets/images/5.jpeg",
+    image: "https://raw.githubusercontent.com/moo-farah/personal-site/main/src/assets/images/5.jpeg",
     date: "July 2024",
-    location: "COEX Aquarium, South Korea",
-    caption: "pregnant korean shark",
+    location: "Abu Dhabi",
+    caption: "Sheikh zayed grand mosque",
     style: "sm:left-[45%] top-8 right-8 rotate-[-3deg] z-30"
   },
   {
     image: "https://raw.githubusercontent.com/moo-farah/personal-site//main/src/assets/images/4.png",
     date: "Noverber 2024",
-    location: "The Mohammed Bin Rashid Library, Dubai",
-    caption: "study mode",
+    location: "Dubai",
+    caption: "The Mohammed Bin Rashid Library",
     style: "top-[10%] left-[70%] rotate-[5deg] z-40"
   }
 ];
